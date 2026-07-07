221df5ae176db4703d513747e3d1e4ad3a30418f
vishalwillbrite <vishalwillbrite@gmail.com>
2026-07-07 18:38:03 +0530

    Train Logistic Regression model

--- train_model.py
+++ train_model.py
@@ -14,6 +14,13 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import (
+    accuracy_score,
+    classification_report,
+    confusion_matrix
+)
 nltk.download("stopwords")
 stemmer = PorterStemmer()
 
@@ -152,3 +159,32 @@ X = vectorizer.fit_transform(news_df["text"])
 print("Feature Matrix Shape:", X.shape)
 print("Target Shape:", y.shape)
 print(vectorizer.get_feature_names_out()[:20])
+# ==========================================
+# Step 13 - Train Test Split
+# ==========================================
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+# ==========================================
+# Step 14 - Create Logistic Regression Model
+# ==========================================
+
+model = LogisticRegression(max_iter=1000)
+# ==========================================
+# Step 15 - Train Model
+# ==========================================
+
+model.fit(X_train, y_train)
+# ==========================================
+# Step 16 - Prediction
+# ==========================================
+
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+
+print("Accuracy:", accuracy)
+print(confusion_matrix(y_test, y_pred))
